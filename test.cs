5519172879c1c01c403d08c8be8ccad68daf2afb
Borz80lvl <nofaki@bk.ru>
2023-10-16 19:04:25 +0300

    Задача 3 исправили ошибки

--- test.cs
+++ test.cs
@@ -4,8 +4,8 @@
 // 1 -> нет
 int week_days = 7;
 Console.WriteLine("Введите число дня недели");
-int week = Int32 Console.ReadLine();
-if ( week < week_days) {
+int week = Convert.ToInt32(Console.ReadLine());
+if (week < 1 || week < week_days) {
     Console.WriteLine("Введенное число не является числом дня недели");
 }
 if (week == 6 || week == 7) {
