0f5929cdf547d2f320781432f62f280bd5b90aea
Borz80lvl <nofaki@bk.ru>
2023-10-17 15:50:37 +0300

    Задача 2 понностью готова

--- test.cs
+++ test.cs
@@ -22,6 +22,7 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+Console.WriteLine("Введите трехначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number / 10 % 10 < 1) {
 Console.WriteLine($"{number} -> третьей цифры нет");
