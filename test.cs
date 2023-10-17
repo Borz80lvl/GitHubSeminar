0f515619981729584b6a4259701fbca3bad2b6a1
Borz80lvl <nofaki@bk.ru>
2023-10-17 16:23:03 +0300

    Задача 2 сделана

--- test.cs
+++ test.cs
@@ -1,3 +1,17 @@
+//Задача 2
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// Console.WriteLine("Введите трехначное число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 100) {
+// Console.WriteLine($"{number} -> третьей цифры нет");
+// }
+// else {
+// Console.WriteLine($"{number} -> {Math.Round(Convert.ToDouble(number / 10 % 10))}");
+// }
+
 //Задача 3
 //  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
@@ -16,18 +30,3 @@
 // {
 //     Console.WriteLine($"{week} -> нет");
 // }
-
-
-// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-Console.WriteLine("Введите трехначное число");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number < 100) {
-Console.WriteLine($"{number} -> третьей цифры нет");
-}
-else {
-Console.WriteLine($"{number} -> {Math.Round(Convert.ToDouble(number / 10 % 10))}");
-}
-
