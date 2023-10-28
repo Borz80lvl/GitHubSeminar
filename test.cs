12aefa8cadae06ec300a26422ff8d339a76b2fdc
Borz80lvl <nofaki@bk.ru>
2023-10-28 17:05:11 +0300

    Файл с домашкой готов к отправке

--- test.cs
+++ test.cs
@@ -1,41 +1,45 @@
-// Console.WriteLine("Напишите число,которое будет возводиться в степень: ");
-// int A = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Напишите степень: ");
-// int B = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine(Math.Pow(A, B));
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+Console.WriteLine("Напишите число,которое будет возводиться в степень: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите степень: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{A},{B} -> {Math.Pow(A, B)}");
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-int[] Array = number.ToString().ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
-int sum = 0;
-for (int i = 0; i < Array.Length; i++)
-{
-    sum = sum + Array[i];
-}
-Console.WriteLine(sum);
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int[] Array = number.ToString().ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
+// int sum = 0;
+// for (int i = 0; i < Array.Length; i++)
+// {
+//     sum = sum + Array[i];
+// }
+// Console.WriteLine($"{number} -> {sum}");
 
  
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-//Задача 3
-//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-// 6 -> да
-// 7 -> да
-// 1 -> нет
-// int week_days = 7;
-// Console.WriteLine("Введите число дня недели");
-// int week = Convert.ToInt32(Console.ReadLine());
-// if (week < 1 || week > week_days) {
-//     Console.WriteLine("Введенное число не является числом дня недели");
-// }
-// if (week == 6 || week == 7) {
-//     Console.WriteLine($"{week} -> да");
-// }
-// else
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// Random random = new Random();
+// int[] numbers = new int[8];
+// for (int i = 0; i < numbers.Length; i++)
 // {
-//     Console.WriteLine($"{week} -> нет");
+//     numbers[i] = random.Next(1, 101);
+// }
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     Console.Write(numbers[i]);
+//     Console.Write("  ");
 // }
